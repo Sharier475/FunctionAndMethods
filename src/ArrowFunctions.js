03c9aa94c3a00eac76887392d27515459006f8d8
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-07 12:46:22 +0600

    basic Function learning and practice complete

---
+++ src/ArrowFunctions.js
@@ -0,0 +1,34 @@
+//Sum function 
+{
+    function sum(x,y) {
+        return x + y;
+    }
+}
+
+//Modern JS Arrow Function
+{
+    const arrowSum = (a,b) => {
+        console.log(a+b);
+    };
+    arrowSum(4,8);
+    
+}
+
+
+//multification function
+{
+    function mul(a,b){
+        return a*b;
+    }
+}
+
+//Modern JS Arrow Function
+{
+    const mul = (a,b) => {
+        console.log(a*b);
+    }
+
+    mul(7,12);
+}
+
+
---
+++ src/function.js
@@ -0,0 +1,31 @@
+
+{
+    function myFunction() {
+        console.log("Welcome to the Reality!");
+        console.log("Don't Worry Just Keep faith on all mighty ALLAH :)");
+    }
+
+    myFunction();
+}
+
+
+// Function -> 2 numbers ,SUM
+{
+    function myFunction(x, y) {
+        console.log(x + y);
+    }
+
+    myFunction(2, 3);
+}
+
+{
+    function sum(x, y) {
+        s = x + y;
+        return s;
+    }
+
+    let val = sum(8, 9);
+    console.log(val);
+
+}
+
