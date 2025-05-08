4ca097bdeda74b3020013ffa9eec0f5fcbf6f514
SHARIER ISLAM <Sharierpulok475@gmail.com>
2025-05-08 11:51:29 +0600

    Add comments to clarify array manipulation methods in MethodPractice.js

--- src/MethodPractice.js
+++ src/MethodPractice.js
@@ -1,7 +1,58 @@
-let arr = [2, 5, 6, 8];
+//By Normal method we use basic calculation.
+{
+    let arr = [2, 5, 6, 8];
 
-arr.forEach(
-    (num) => {
-        console.log(num * num);
+    arr.forEach(
+        (num) => {
+            console.log(num * num);
+        }
+    );
+}
+
+
+//Map Method basically we use map to create a new array. 
+{
+    let num = [32,67,48];
+
+   let newArr = num.map(
+        (val) =>{
+            return val*val;
+        }
+    );
+    console.log(newArr);
+}
+
+
+//Filter Method
+{
+    let arr1 = [34,35,32,67,56,98,45];
+    let evenArray = arr1.filter((num) =>
+    {
+        return num % 2 === 0;
     }
 );
+console.log(evenArray);
+}
+
+//Reduce Method
+
+{
+    let arr2 = [1,2,34,,56,6];
+    const output = arr2.reduce(
+        (prev,curr) =>{
+            return prev + curr;
+        }
+    );
+    console.log(output);
+}
+
+// Using reduce method to get smallest number.
+{
+    let arr2 = [1,2,34,,56,6];
+    const output = arr2.reduce(
+        (prev,curr) =>{
+            return prev > curr ? prev : curr;
+        }
+    );
+    console.log(output);
+}
